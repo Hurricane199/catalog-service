dd43a700adaf15385ad0741681e46548d2033232
Sam <130892836+Hurricane199@users.noreply.github.com>
2026-09-03 13:09:41 +0800

    TASK-1001: Настроил конфигурацию и окружение (#1)
    * TASK-1001: Настроил конфигурацию и окружение
    
    * TASK-1001: fix: исправление замечаний линтера и go mod
    
    * TASK-1001: fix: исправление опечаток в env и env.example

---
+++ internal/app/config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
+
+	"github.com/Hurricane199/catalog-service/internal/app/config/section"
+)
+
+type Config struct {
+	Repository section.Repository
+	Processor  section.Processor
+	Monitor    section.Monitor
+}
+
+var Root Config
+
+func Load() {
+	if err := godotenv.Load(); err != nil {
+		log.Print(err.Error())
+	}
+
+	if err := envconfig.Process("APP", &Root); err != nil {
+		log.Fatal(err.Error())
+	}
+}
